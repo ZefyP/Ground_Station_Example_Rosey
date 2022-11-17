5550984992f80bcda52fc26eb2cd3328c571f44f
ZefyP <zefypi@gmail.com>
2022-11-17 23:56:24 +0100

    improved file names

--- serial_receive.py
+++ serial_receive.py
@@ -1,61 +1,24 @@
 import time
 import serial # import serial library
 from string import digits
-import matplotlib.pyplot as plt # import matpltlib library
-from drawnow import *
 
-# create a file to write and read from
-# results_file = open("data.txt","a")
+results_file = open("data.txt","a")
 
 # get the start time
 start_time = time.time()
 
-time = []
-distanceF = []
 arduinoSerialData = serial.Serial('com5', 115200)
-plt.ion() # tell matplotlib that interactive mode is on to plot the live data
-cnt = 0 # create counter
+while (1==1):
+    if (arduinoSerialData.inWaiting()>0):        
+        # get the runtime
+        current_time = time.time()
+        elapsed_time= current_time - start_time # get runtime in seconds
+        elapsed_time = f"{elapsed_time:.2f}"    # limit float precision to 2 decimals
 
+        results_file.write( str(elapsed_time) + ',')
+        myData = arduinoSerialData.readline() # reads everything as a string
+        myData = myData.decode('utf-8').strip() # decode received data. clean digit string
+        results_file.write(str(myData)+ '\n')
+        print (myData)
 
-
-def makeFig(): # create a function that makes the desired plot
-    # plt.ylim(80,90)
-    plt.title('Data live stream')
-    plt.grid(True)
-    plt.ylabel('Sensor Data')
-    plt.plot(distanceF,'ro-', label = 'Zefy')
-    plt.legend(loc='upper left')
-    plt.xlabel('Time (seconds)')
-
-
-while True: # loop for forever
-    while (arduinoSerialData.inWaiting() == 0): # Wait here until there is data
-       pass #do nothing
-
-    myData = arduinoSerialData.readline()       # reads everything as a string
-    myData = myData.decode('utf-8').strip()     # decode received data. clean digit string
-    
-    print (myData) # print the data on the terminal
-
-    dataArray = myData.split(',')
-    distance = float( dataArray[0] )
-    distanceF.append(distance)
-
-
-    # for eachLine in myData:
-    #     if len(eachLine)>1:
-    #         x,y = eachLine.split(',')
-    #         time.append(float(x))
-    #         distance.append(float(y))
-    
-    drawnow(makeFig) # call drawnow to update the live graph
-    plt.pause(.000001)
-    cnt = cnt+1 # increment
-    if(cnt>50):
-        distanceF.pop(0)# get rid of the first element in the array from the plot
-# results_file.close()
-
-# get the execution time
-end_time = time.time()
-elapsed_time = end_time - start_time
-print('execution time: ', elapsed_time, 'seconds')
+results_file.close()
