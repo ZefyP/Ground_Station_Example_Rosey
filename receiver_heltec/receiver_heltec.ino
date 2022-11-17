934c52798f6126cf9d9005202c05136bfded4fd2
ZefyP <zefypi@gmail.com>
2022-11-17 16:01:11 +0100

    supporting scripts for heltec and radiolib

---
+++ receiver_heltec/receiver_heltec.ino
@@ -0,0 +1,63 @@
+/*
+  Using Heltec.LoRa received data display in OLED and transmit the packets to serial port.
+
+  The onboard OLED display is SSD1306 driver and I2C interface. In order to make the
+  OLED correctly operation, you should output a high-low-high(1-0-1) signal by soft-
+  ware to OLED's reset pin, the low-level signal at least 5ms.
+
+  OLED pins to ESP32 GPIOs via this connecthin:
+  OLED_SDA -- GPIO4
+  OLED_SCL -- GPIO15
+  OLED_RST -- GPIO16
+
+*/
+#include "heltec.h" 
+
+#define BAND    868E6  //you can set band here directly,e.g. 868E6,915E6
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+void OLED_display_LoRaData(){
+  Heltec.display->clear();
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
+  Heltec.display->drawStringMaxWidth(0 , 26 , 128, packet);
+  Heltec.display->drawString(0, 0, rssi+ " db");  
+  Heltec.display->display();
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  OLED_display_LoRaData();
+  Serial.println(packet);
+}
+
+void setup() { 
+  //init serial communication 
+  Serial.begin(115200);
+
+  //init radio
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+ 
+  Heltec.display->init(); 
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->clear();
+  
+  Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
+  Heltec.display->drawString(0, 10, "Wait for incoming data...");
+  Heltec.display->display();
+  delay(1000);
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
+}
--- receiver_radiolib/receiver_radiolib.ino
+++ receiver_radiolib/receiver_radiolib.ino
@@ -1,115 +1,162 @@
 /*
-Creator: Zefy Pissaki
-RadioLib SX1276 Receive 
+  This is a simple example show the Heltec.LoRa recived data in OLED.
 
-This example listens for LoRa transmissions from multiple SX1276 Lora modules.
-To successfully receive data, the following settings have to be the same
-on both transmitter and receiver:
-- carrier frequency
-- bandwidth
-- spreading factor
-- coding rate
-- sync word
-- preamble length
+  The onboard OLED display is SSD1306 driver and I2C interface. In order to make the
+  OLED correctly operation, you should output a high-low-high(1-0-1) signal by soft-
+  ware to OLED's reset pin, the low-level signal at least 5ms.
+
+  OLED pins to ESP32 GPIOs via this connecthin:
+  OLED_SDA -- GPIO4
+  OLED_SCL -- GPIO15
+  OLED_RST -- GPIO16
+
 */
+#include "heltec.h" 
 
-// include the library
-#include <RadioLib.h>
-#include <Arduino.h>
+#define BAND    868E6  //you can set band here directly,e.g. 868E6,915E6
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
 
-// Create Modules for the number of student radios you expect
+void OLED_display_LoRaData(){
+  Heltec.display->clear();
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
+  Heltec.display->drawStringMaxWidth(0 , 26 , 128, packet);
+  Heltec.display->drawString(0, 0, rssi+ " db");  
+  Heltec.display->display();
+}
 
-// SX1276 has the following connections:
-// NSS pin:   18
-// DIO0 pin:  26
-// RESET pin: 14
-// DIO1 pin:  not found
-SX1278 radio = new Module(18, 26, 14);
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  OLED_display_LoRaData();
+  Serial.println(packet);
+}
 
+void setup() { 
+  //init serial communication 
+  Serial.begin(115200);
 
-// SET YOUR SYNC WORD
-uint8_t my_password = 'A'; // if you're transmitting, change this to your letter of preference from A to P  
-int state;
-String str;
-
-void setup() {
-    Serial.begin(115200);
-
-    // initialise the radio chip SX1276 with default settings
-    Serial.print(F("[SX1276] Initialising ... "));
-    state = radio.begin();
+  //init radio
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+ 
+  Heltec.display->init(); 
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->clear();
+  
+  Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
+  Heltec.display->drawString(0, 10, "Wait for incoming data...");
+  Heltec.display->display();
+  delay(1000);
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
 }
 
 void loop() {
-
-    // Every 10 milliseconds divide with 16 places, obtaining an incremental step. 
-    // The remaining will be added to the decimal value 65 (of the char: 'A').  
-    // This results in a sweeped sync word setting from 65(char : 'A') to 80 (char: 'P')/
-    my_password = char((millis()/10%16)+ 65); // 65 is the Decimal value of char A from ASCII table 
-
-    
-    Serial.println(char(my_password));
-    if (radio.setSyncWord(my_password) != RADIOLIB_ERR_NONE) { 
-        //if something went wrong
-        Serial.println(F("Unable to set sync word!"));
-        while (true);
-    }
-
-
-    // Returns error code. Wrap this to a function later: 
-    if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("Initialisation success!"));
-    } else {
-    Serial.print(F("failed, state code "));
-    Serial.println(state); // in the future add message for the rest of the error codes 
-    while (true);
-    }
-
-    Serial.print(F("[SX1276] Waiting for incoming transmission ... "));
-
-    // you can receive data as an Arduino String
-    int state = radio.receive(str);
-  
-  
-    if (state == RADIOLIB_ERR_NONE) {
-      // packet was successfully received
-      Serial.println(F("success!"));
-  
-      // print the data of the packet
-      Serial.print(F("[SX1276] Data:\t\t\t"));
-      Serial.println(str);
-  
-      // print the RSSI (Received Signal Strength Indicator)
-      // of the last received packet
-      Serial.print(F("[SX1276] RSSI:\t\t\t"));
-      Serial.print(radio.getRSSI());
-      Serial.println(F(" dBm"));
-  
-      // print the SNR (Signal-to-Noise Ratio)
-      // of the last received packet
-      Serial.print(F("[SX1276] SNR:\t\t\t"));
-      Serial.print(radio.getSNR());
-      Serial.println(F(" dB"));
-  
-      // print frequency error
-      // of the last received packet
-      Serial.print(F("[SX1276] Frequency error:\t"));
-      Serial.print(radio.getFrequencyError());
-      Serial.println(F(" Hz"));
-  
-      } else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
-      // timeout occurred while waiting for a packet
-      Serial.println(F("timeout!"));
-  
-      } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-      // packet was received, but is malformed
-      Serial.println(F("CRC error!"));
-
-    } else {
-      // some other error occurred
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-      Serial.println(my_password);
-
-    }//end of error checks
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
 }
+
+
+
+
+
+
+
+
+//
+//
+////-----------------------
+//// include the library
+//#include <RadioLib.h>
+//
+//// SX1278 has the following connections:
+//// NSS pin:   10
+//// DIO0 pin:  2
+//// RESET pin: 9
+//// DIO1 pin:  3
+//SX1276 radio = new Module(18, 26, 14);
+//// or using RadioShield
+//// https://github.com/jgromes/RadioShield
+////SX1278 radio = RadioShield.ModuleA;
+//
+//void setup() {
+//  Serial.begin(115200);
+//
+//  // initialize SX1278 with default settings
+//  Serial.print(F("[SX1276] Initialising ... "));
+//  int state = radio.begin();
+//  if (state == RADIOLIB_ERR_NONE) {
+//    Serial.println(F("success!"));
+//  } else {
+//    Serial.print(F("failed, code "));
+//    Serial.println(state);
+//    while (true);
+//  }
+//}
+//
+//void loop() {
+//  Serial.print(F("[SX1276] Waiting for incoming transmission ... "));
+//
+//  // you can receive data as an Arduino String
+//  // NOTE: receive() is a blocking method!
+//  //       See example ReceiveInterrupt for details
+//  //       on non-blocking reception method.
+//  String str;
+//  int state = radio.receive(str);
+//
+//  // you can also receive data as byte array
+//  /*
+//    byte byteArr[8];
+//    int state = radio.receive(byteArr, 8);
+//  */
+//
+//  if (state == RADIOLIB_ERR_NONE) {
+//    // packet was successfully received
+//    Serial.println(F("success!"));
+//
+//    // print the data of the packet
+//    Serial.print(F("[SX1276] Data:\t\t\t"));
+//    Serial.println(str);
+//
+//    // print the RSSI (Received Signal Strength Indicator)
+//    // of the last received packet
+//    Serial.print(F("[SX1276] RSSI:\t\t\t"));
+//    Serial.print(radio.getRSSI());
+//    Serial.println(F(" dBm"));
+//
+//    // print the SNR (Signal-to-Noise Ratio)
+//    // of the last received packet
+//    Serial.print(F("[SX1276] SNR:\t\t\t"));
+//    Serial.print(radio.getSNR());
+//    Serial.println(F(" dB"));
+//
+//    // print frequency error
+//    // of the last received packet
+//    Serial.print(F("[SX1276] Frequency error:\t"));
+//    Serial.print(radio.getFrequencyError());
+//    Serial.println(F(" Hz"));
+//
+//  } else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
+//    // timeout occurred while waiting for a packet
+//    Serial.println(state+ "----");
+//    Serial.println(F("timeout!"));
+//
+//  } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+//    // packet was received, but is malformed
+//    Serial.println(F("CRC error!"));
+//
+//  } else {
+//    // some other error occurred
+//    Serial.print(F("failed, code "));
+//    Serial.println(state);
+//
+//  }
+//
+//  delay(100);
+//}
