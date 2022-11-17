eb534d7af9e8dff2757bd40cad2757a5c73c0e8d
ZefyP <zefypi@gmail.com>
2022-11-17 22:23:39 +0100

    test

--- serial_receive.py
+++ serial_receive.py
@@ -1,11 +1,15 @@
 import time
 import serial # import serial library
+from string import digits
 
-results_file = open("datain.txt","a")
+results_file = open("data.txt","a")
 
 arduinoSerialData = serial.Serial('com5', 115200)
 while (1==1):
     if (arduinoSerialData.inWaiting()>0):
         myData = arduinoSerialData.readline() # reads everything as a string
-        results_file.write(str(myData))
+        myData = myData.decode('utf-8').strip() # decode received data. clean digit string
+        results_file.write(str(myData)+ '\n')
         print (myData)
+
+results_file.close()
